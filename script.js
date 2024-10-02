89cc39b297e80751a76cc4683ea0b9631b9c6ac5
Marco CARPENTIER <marcarp@protonmail.com>
2024-10-02 18:22:39 +0200

    Add getter

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let gameOn = false;
 let playerOneName, playerTwoName;
 // DOM
 const popinName = document.getElementsByClassName("modal-bg")[0];
+const fieldName = document.getElementById("player-name");
 
 function init() {
  popinName.classList.add("active");
